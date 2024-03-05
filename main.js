eb0f46b9ebe6459f3643be8d29b3b7aca7ca1e80
sharaf <sharafahmed0303@gmail.com>
2024-03-06 00:00:49 +0530

    animation updated

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".position",{
+    strings:["Frontend Developer","Backend Developer","Android Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
